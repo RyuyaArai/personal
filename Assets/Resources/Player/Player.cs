ff4b8c06041c901079aa795cf8b602fb066f5a3a
RyuyaArai <k020g1223@g.neec.ac.jp>
2022-11-09 16:00:31 +0900

    pl挙動+カメラ回転

--- Assets/Resources/Player/Player.cs
+++ Assets/Resources/Player/Player.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 4;
+    public Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody=GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W)){
+            rigidbody.velocity = transform.forward * speed;;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            rigidbody.velocity = -transform.right * speed;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            rigidbody.velocity = -transform.forward * speed;;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            rigidbody.velocity = transform.right * speed;;
+        }
     }
 }
